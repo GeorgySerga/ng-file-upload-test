7ca4fba14531038f1717226ed8d7c3839d0d2fee
GeorgySerga <georgy.serga@gmail.com>
2015-10-04 16:39:07 +0300

    Rewrite script.js, no more special progress and allFiles arrays

--- script.js
+++ script.js
@@ -2,81 +2,68 @@ var app = angular.module('fileUpload', ['ngFileUpload']);
 
 app.controller('MyCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
 
-  $scope.filesToUpload = [];
-  $scope.allFiles = [];
-  $scope.progress = [];
+  $scope.files = [];
+  $scope.filesToUpload = 0;
   var isInProgress = false;
   var upload;
 
-  $scope.updateFileList = function(files) {
-    if (files) {
-      for (var i = 0; i < files.length; i++) {
-        $scope.filesToUpload.push(files[i]);
-        $scope.allFiles.push(files[i]);
-        $scope.progress.push(0);
+  $scope.updateFileList = function(files, newFiles) {
+    if (newFiles) {
+      $scope.files = $scope.files.concat(newFiles);
+      $scope.filesToUpload += newFiles.length;
+      var i = $scope.files.length - newFiles.length;
+      for (i; i < $scope.files.length; i++) {
+        $scope.files[i].progress = 0;
+        $scope.files[i].status = true;
       }
     }
-    if ($scope.filesToUpload.length) {
-      if (isInProgress === false) {
-        isInProgress = true;
-        $scope.uploadFiles($scope.filesToUpload);
+    if (!isInProgress) {
+      if ($scope.filesToUpload) {
+        if ($scope.files[$scope.files.length - $scope.filesToUpload].status) {
+          isInProgress = true;
+          $scope.uploadFiles($scope.files[$scope.files.length - $scope.filesToUpload]);
+        } else {
+          $scope.filesToUpload--;
+          $scope.updateFileList();
+        }
       }
-    } else {
-      isInProgress = false;
     }
   };
 
-  $scope.uploadFiles = function(files) {
-    var index = $scope.allFiles.indexOf(files[0]);
-    if (files && files.length) {
-      upload = Upload.upload({
-        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
-        data: {
-           file: files[0]
-        }
-      }).progress(function (evt) {
-        $scope.progress[index] = parseInt(100.0 * evt.loaded / evt.total);
-      }).success(function (status) {
-        $scope.filesToUpload.shift();
-        isInProgress = false;
-        $scope.updateFileList();
-      });
-    }
+  $scope.uploadFiles = function(file) {
+    upload = Upload.upload({
+      url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+      data: {
+         file: file
+      }
+    }).progress(function (evt) {
+      file.progress = parseInt(100.0 * evt.loaded / evt.total);
+    }).success(function (status) {
+      isInProgress = false;
+      $scope.filesToUpload--;
+      $scope.updateFileList();
+    });
   };
 
   $scope.abort = function() {
-    $scope.filesToUpload.length = 0;
     upload.abort();
     isInProgress = false;
-    var index = $scope.progress.indexOf(0);
-    if (index !== -1) {
-      if ($scope.progress[index - 1] < 100) {
-        $scope.progress = $scope.progress.splice(0, index - 1);
-        $scope.allFiles = $scope.allFiles.splice(0, index - 1);
-      } else {
-        $scope.progress = $scope.progress.splice(0, index);
-        $scope.allFiles = $scope.allFiles.splice(0, index);
+    $scope.filesToUpload = 0;
+    for (var i = 0; i < $scope.files.length; i++) {
+      if ($scope.files[i].progress < 100) {
+        $scope.files[i].status = false;
       }
-    } else {
-      $scope.progress = $scope.progress.splice(0, $scope.progress.length - 1);
-      $scope.allFiles = $scope.allFiles.splice(0, $scope.allFiles.length - 1);
     }
   };
 
   $scope.abortForOneFile = function(index) {
-    var fileName = $scope.allFiles[index];
-    if ($scope.progress[index] !== 0) {
+    if ($scope.files[index].progress) {
       upload.abort();
-      $scope.progress.splice(index, 1);
-      $scope.allFiles.splice(index, 1);
-      $scope.filesToUpload.shift();
+      $scope.files[index].status = false;
       isInProgress = false;
       $scope.updateFileList();
     } else {
-      $scope.progress.splice(index, 1);
-      $scope.allFiles.splice(index, 1);
-      index = $scope.filesToUpload.indexOf(fileName);
-      $scope.filesToUpload.splice(index, 1);
+      $scope.files[index].status = false;
     }
   };
 }]);
